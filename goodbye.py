d16ffa6f598c302452456f7c4e292104971b8a81
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2013-05-26 15:14:39 -0700

    update var name, print stmts to methods

--- goodbye.py
+++ goodbye.py
@@ -12,22 +12,22 @@ POLL_INTERVAL_SECS = 60*5
 def main():
     config = {}
     try:
-        with open(AUTH_CONFIG_FILE) as configfile:
-            config = json.loads(configfile.read())
+        with open(AUTH_CONFIG_FILE) as config_file:
+            config = json.loads(config_file.read())
     except IOError:
-        print 'Twitter authentication credentials not found.'
-        print 'Step 1: Create a Twitter app https://dev.twitter.com/apps/new'
-        print 'Step 2: Go to OAuth Tool tab, copy consumer keys:'
+        print('Twitter authentication credentials not found.')
+        print('Step 1: Create a Twitter app https://dev.twitter.com/apps/new')
+        print('Step 2: Go to OAuth Tool tab, copy consumer keys:')
         config['CONSUMER_KEY'] = raw_input('Paste Consumer key: ')
         config['CONSUMER_SECRET'] = raw_input('Paste Consumer secret: ')
 
-        print '\nStep 3: Go to Details tab, press button "create access token"'
-        print 'Step 4: Copy generated access tokens:'
+        print('Step 3: Go to Details tab, press button "create access token"')
+        print('Step 4: Copy generated access tokens:')
         config['ACCESS_TOKEN'] = raw_input('Paste Access Token: ')
         config['ACCESS_SECRET'] = raw_input('Paste Access Token secret: ')
 
-        with open(AUTH_CONFIG_FILE, 'w') as configfile:
-            configfile.write(json.dumps(config))
+        with open(AUTH_CONFIG_FILE, 'w') as config_file:
+            config_file.write(json.dumps(config))
 
     auth = tweepy.OAuthHandler(config['CONSUMER_KEY'],
                                config['CONSUMER_SECRET'])
@@ -35,12 +35,12 @@ def main():
     api = tweepy.API(auth)
 
     me = api.me()
-    print 'Welcome, %s! You have %d followers.' % (me.screen_name,
-                                                   me.followers_count)
+    print('Welcome, %s! You have %d followers.' % (me.screen_name,
+                                                   me.followers_count))
 
     prev_follower_ids = get_followers_ids(api)
-    print 'This will check every %d seconds until someone unfollows...' \
-        % POLL_INTERVAL_SECS
+    print('This will check every %d seconds until someone unfollows...'
+          % POLL_INTERVAL_SECS)
 
     while True:
         time.sleep(POLL_INTERVAL_SECS)
@@ -51,11 +51,11 @@ def main():
         for unfollower_id in diff:
             user = api.get_user(unfollower_id)
             tweet = send_mention(api, user)
-            print '@%s (%s), sent mention http://twitter.com/%s' \
-                % (user.screen_name, user.name, user.screen_name)
+            print('@%s (%s), sent mention http://twitter.com/%s'
+                  % (user.screen_name, user.name, user.screen_name))
 
         if diff:
-            print 'You have %d followers.' % len(new_follower_ids)
+            print('You have %d followers.' % len(new_follower_ids))
 
         prev_follower_ids = new_follower_ids
 
